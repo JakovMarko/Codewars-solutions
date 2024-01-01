607e7af7049dd5a62bacf4901808d44179ec8fb8
Marko <119257070+JakovMarko@users.noreply.github.com>
2024-01-01 13:11:22 +0100

    added

---
+++ sortedYesNoHow.js
@@ -0,0 +1,25 @@
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+function isSortedAndHow(array) {
+    let ascending = array.slice().sort((a,b) => a-b).join('');
+    let descending = array.slice().sort((a,b) => b-a).join('');
+    let arrayString = array.join('');
+    if(arrayString == ascending){
+      return 'yes, ascending';
+    } else if(arrayString == descending){
+      return 'yes, descending';
+    } else {
+      return 'no';
+    }
+  }
+
+function isSortedAndHowTwo(array){
+    return array.every((x, i)=> i == 0 || array[i] >= array[i-1])?
+    'yes, ascending' : array.every((x, i) => i==0 || array[i] <= array[i-1])?
+    'yes, descending' : 'no';
+}
