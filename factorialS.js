3585d07539456d144015430ca06529f0eaf15073
Marko <119257070+JakovMarko@users.noreply.github.com>
2024-02-22 19:23:08 +0100

    added

---
+++ factorialS.js
@@ -0,0 +1,15 @@
+// Your task is to write function factorial.
+
+// https://en.wikipedia.org/wiki/Factorial
+
+
+
+function factorial(n){
+    let result =1 ;
+
+    for(let i = 1; i <=n; i++){
+        result = result * i;
+    }
+
+    return result;
+}
