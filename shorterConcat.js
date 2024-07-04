264c4199c27926ade29e795b322e19306e987270
Marko <119257070+JakovMarko@users.noreply.github.com>
2024-07-04 22:57:27 +0200

    added

--- shorterConcat.js
+++ shorterConcat.js
@@ -1,5 +1,5 @@
 // Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
-
+//
 // In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
 
 // Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
