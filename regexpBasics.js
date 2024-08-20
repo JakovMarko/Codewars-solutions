5ca0ab93008a2d7369b21696a7cd5a9b90c1d659
Marko <119257070+JakovMarko@users.noreply.github.com>
2024-08-20 19:44:36 +0200

    added

--- regexpBasics.js
+++ regexpBasics.js
@@ -1,5 +1,11 @@
-// Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
+// Definition
+// A number is a Special Number if it’s digits only consist 0, 1, 2, 3, 4 or 5
 
-String.prototype.vowel = function () {
-  return /^[aeiou]$/i.test(this);
-};
+// Given a number determine if it special number or not .
+
+// Warm-up (Highly recommended)
+// Playing With Numbers Series
+
+function specialNumber(n) {
+  return /[6-9]/.test(n) ? "NOT!!" : "Special!!";
+}
